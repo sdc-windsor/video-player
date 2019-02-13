d5a506f30e06b9eb49b5e6ce8c08616653330c91
Chase Kallemeyn <chase.kallemeyn@gmail.com>
2019-02-12 23:14:20 -0600

    Adds dependencies and server to service

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const morgan = require('morgan');
+const port = process.env.PORT | 3000;
+
+app.use(express.static('./public/dist'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+app.get('/videoplayer/:video_id', (req, res) => {
+  
+});
+

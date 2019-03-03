2011c11cbb758a5f95dd055c67ff01d06177a358
Chase Kallemeyn <chase.kallemeyn@gmail.com>
2019-03-02 22:25:57 -0600

    preparing for EBS deployment

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -17,7 +17,6 @@ export default class App extends Component {
 
   componentDidMount() {
     let id = window.location.pathname;
-    console.log('****ID IN COMPONENTDIDMOUNT****', id);
     (id === '/') ? id = '/1' : id;
     this.retrieveData(id);
   }
@@ -25,7 +24,6 @@ export default class App extends Component {
   retrieveData(id) {
     return axios.get(`http://localhost:3000/videos${id}`)
       .then((response) => {
-        console.log('SUCCESS YOU RETRIEVED THE DATA', response.data);
         const { video_url, title } = response.data[0];
         this.setState({
           video_url,

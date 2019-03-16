a51110d746cd9089aab6673aa01faf7a0ff6dcd8
akwong372 <akwong372@gmail.com>
2019-03-15 20:42:39 -0700

    Add separate files for sdc seeding script

--- db/seed-db.js
+++ db/seed-db.js
@@ -1,24 +1,31 @@
-const videoData = require('../helpers/data.js');// array of video data
+const videoData = require('../helpers/video-data');// array of video data
 const db = require('../db/index.js');
 
-db.schema.dropTableIfExists('videos')
-  .then(() => {
-    return db.schema.createTable('videos', (t) => {
-      t.increments('id').primary();
-      t.string('video_url', 255);
-      t.text('thumbnail');
-      t.string('title', 50);
-      t.string('author', 50);
-      t.integer('plays');
-    })
-      .then(() => db('videos').insert(videoData))
-      .then(() => {
-        console.log('database seeded');
-        process.exit();
-      })
+db.schema.dropTableIfExists('videos').then((exists) => {
+  if (!exists) {
+    console.log('error in dropping table', exists);
+    return;
+  }
+  return db.schema.createTable('videos', (t) => {
+    t.increments('id').primary();
+    t.string('video_url', 255);
+    t.text('thumbnail');
+    t.string('title', 50);
+    t.string('author', 50);
+    t.integer('plays');
+  }).then(() => {
+    return db('videos').insert(videoData)
       .catch((err) => {
         console.log('found an error in db seeding', err);
+      }).then(() => {
+        return db.select('*').from('videos').then((rows) => {
+          if (!rows) {
+            console.log('rows not found', rows);
+          }
+          console.log('Success! Here are the rows:', rows);
+        });
       });
   });
+});
 
 module.exports = db;

acff754e5dcdf8b1e7801711df82c0e4df29d80b
akwong372 <akwong372@gmail.com>
2019-04-25 02:19:13 -0700

    Fix thumbnails endpoint

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -23,7 +23,8 @@ export default class App extends Component {
   }
 
   retrieveData(id) {
-    const url = 'http://127.0.0.1:3001';
+    // const url = 'http://127.0.0.1:3001';
+    const url = `http://${window.location.hostname}:3001`;
     return axios.get(`${url}/videos${id}`)
       .then((response) => {
         const { video_url, title } = response.data[0];
